4874ec2a164f536526764b36c28231e8a99aa3e8
Ahmad Fajar & Fickry <pair+ahmad-fajar.biliman@devbootcamp.com>
2017-06-08 22:25:45 +0700

    perbaikan. ada yang kurang

--- todo.js
+++ todo.js
@@ -17,11 +17,15 @@ class View {
     console.log('_______________________________________________________________');
     console.log('                            Help Menu                          ');
     console.log('‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾');
-    console.log(' list                            Show all ToDo list'              );
-    console.log(' add <task>                      Adding new task'                 );
-    console.log(' delete <task_id>                Delete task by Id'               );
-    console.log(' completed <task_id>             Mark task as complete by Id'    );
-    console.log(' uncompleted <task_id>           Mark task as uncompeleted by Id' );
+    console.log(' add <task>                    Adding new task'                );
+    console.log(' addtag <task_id tag>          Adding new task'                );
+    console.log(' delete <task_id>              Delete task by Id'              );
+    console.log(' completed <task_id>           Mark task as complete by Id'    );
+    console.log(' completedlist                 Order by updatedAt'             );
+    console.log(' list                          Show all ToDo list'             );
+    console.log(' listbytag <tag>               View by tag'                    );
+    console.log(' uncompleted <task_id>         Mark task as uncompeleted by Id');
+    console.log(' uncompletedlist               Order by createdAt'             );
     console.log('_______________________________________________________________');
   }
 
@@ -29,16 +33,52 @@ class View {
 
   displayList() {
     console.log('\n');
-    db.Todo.read(Todos => {
-      Todos.forEach(Todos => {
-        if (Todos.completed === true) {
-          console.log(`${Todos.id}. ${Todos.task} ✓✓`);
+    db.Todo.findAll({order: [['id', 'ASC']]})
+    .then(list => {
+      list.forEach(Todo => {
+        if (Todo.completed === true) {
+          console.log(`${Todo.id}. Task: ${Todo.task}. Tag: ${Todo.tag} ✓✓`);
         } else {
-          console.log(`${Todos.id}. ${Todos.task} ✘✘`);
+          console.log(`${Todo.id}. Task: ${Todo.task}. Tag: ${Todo.tag} ✘✘`);
         }
       })
     })
+
   } // displayList
+
+  displayListCompleted() {
+    console.log('\n');
+    db.Todo.findAll({order: [['updatedAt', 'ASC']]})
+    .then(list => {
+      list.forEach(Todos => {
+        if (Todos.completed === true) {
+          console.log(`${Todos.id}. ${Todos.task} ✓✓ ${Todos.updatedAt}`);
+        }
+      })
+    })
+  }
+
+  displayListUncompleted() {
+    console.log('\n');
+    db.Todo.findAll({order: [['createdAt', 'ASC']]})
+    .then(list => {
+      list.forEach(Todos => {
+        if (Todos.completed === false) {
+          console.log(`${Todos.id}. ${Todos.task} ✘✘ ${Todos.createdAt}`);
+        }
+      })
+    })
+  }
+
+  displayByTag(contentOrId) {
+    console.log(('\n'));
+    db.Todo.findAll({where : {tag : contentOrId}})
+    .then(list => {
+      list.forEach(Todos => {
+        console.log(`${Todos.id}. ${Todos.task}`);
+      })
+    })
+  }
 
   displayAdd(todoTask) {
     console.log('\n');
@@ -57,6 +97,15 @@ class View {
       });
   } // displayAdd
 
+  displayAddTag(contentOrId, tag) {
+    db.Todo.update({
+      tag : tag
+    },{
+      where : {
+        id : id
+      }
+    })
+  }
 
   displayDelete(todoId) {
     console.log('\n');
@@ -127,6 +176,14 @@ class Controller {
       return this._view.displayMark(contentOrId, status);
     } else if (option == 'uncompleted') {
       return this._view.displayMark(contentOrId, status);
+    } else if (option == 'completedlist') {
+      return this._view.displayListCompleted();
+    } else if (option == 'uncompletedlist') {
+      return this._view.displayListUncompleted();
+    } else if (option == 'listbytag') {
+      return this._view.displayByTag(contentOrId);
+    } else if (option == 'addtag') {
+      return this._view.displayAddTag(contentOrId, tag);
     } else {
       return this._view.displayError();
     }
@@ -136,6 +193,7 @@ class Controller {
 let argv =process.argv;
 let option = argv[2];
 let contentOrId = argv[3];
+let tag = argv.slice(3).join(' ');
 let controller = new Controller();
 
 controller.executeMenu(option, contentOrId)
